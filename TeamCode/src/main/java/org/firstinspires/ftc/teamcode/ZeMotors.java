83f1eeb115d6b2dc44788d4d2a89bc3817a8b346
WESTIN LAMBERT <LAMBERT46313@students.apsb.org>
2026-01-16 21:52:37 -0600

    new code that makes the robot work trust

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZeMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZeMotors.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
+
 @TeleOp(name = "You Drive")
 public class ZeMotors extends LinearOpMode {
     @Override
@@ -19,8 +21,25 @@ public class ZeMotors extends LinearOpMode {
         // Rhino wheel setup
         DcMotor flywheel = hardwareMap.get(DcMotor.class, "flywheel");
 
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        boolean isLeftTriggerDown = false;
         waitForStart();
         while (this.opModeIsActive()) {
+
+            if (gamepad1.left_trigger >= 0.5) {
+                isLeftTriggerDown = true;
+            }
+
+            else { isLeftTriggerDown = false; }
 
             // Turn on intake
             if (gamepad1.right_trigger >= 0.5) {
@@ -76,9 +95,84 @@ public class ZeMotors extends LinearOpMode {
                 intake.setPower(0);
                 geckoWheel.setPower(0);
             }
+            // bootleg drivtrain code i made right before comp
+            if (gamepad1.dpad_up ) {
+                frontLeft.setPower(-1);
+                frontRight.setPower(1);
+                backRight.setPower(-1);
+                backLeft.setPower(1);
+            }
+            else {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backRight.setPower(0);
+                backLeft.setPower(0);
+            }
+            if (gamepad1.dpad_down) {
+                frontLeft.setPower(1);
+                frontRight.setPower(-1);
+                backRight.setPower(1);
+                backLeft.setPower(-1);
+            }
+            else {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backRight.setPower(0);
+                backLeft.setPower(0);
+            }
+
+            if (gamepad1.dpad_left) {
+                frontLeft.setPower(1);
+                frontRight.setPower(1);
+                backRight.setPower(-1);
+                backLeft.setPower(-1);
+            }
+
+            else {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backRight.setPower(0);
+                backLeft.setPower(0);
+            }
+
+            if (gamepad1.dpad_right) {
+                frontLeft.setPower(-1);
+                frontRight.setPower(-1);
+                backRight.setPower(1);
+                backLeft.setPower(1);
+                }
+
+            else {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backRight.setPower(0);
+                backLeft.setPower(0);
+            }
+
+            if (gamepad1.right_bumper && isLeftTriggerDown) {
+                intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                geckoWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                flywheel.setPower(1);
+                geckoWheel.setPower(1);
+                intake.setPower(1);
+            }
+
+            else if (gamepad1.right_bumper) {
+
+                flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                flywheel.setPower(1);
+            }
+
+            else {
+                flywheel.setPower(0);
+                geckoWheel.setPower(0);
+                intake.setPower(0);
+            }
+
+            }
 
         }
 
     }
-
-}
